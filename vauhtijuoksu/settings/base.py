b0edd40463572bd428026f2a0b1510b77af81e53
Uula Ranta <uula.ranta@gmail.com>
2022-02-11 16:09:16 +0200

    Rename generic css & templates

--- vauhtijuoksu/settings/base.py
+++ vauhtijuoksu/settings/base.py
@@ -100,7 +100,7 @@ TEMPLATES = [
 
 # List of templates that can be used for CMS pages
 CMS_TEMPLATES = [
-    ('vauhtijuoksu/vjGeneric.html', 'VJ Generic theme'),
+    ('vauhtijuoksu/vauhtijuoksu.html', 'VJ Generic theme'),
     ('vauhtijuoksu/vj2021plus.html', 'VJ 2021+ theme'),
     ('vauhtijuoksu/vj2021plus_fullscreen.html', 'VJ 2021+ fullscreen theme'),
     ('vauhtijuoksu/vj2021.html', 'VJ 2021 theme'),
