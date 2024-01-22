08d47cec2d05e2a95ebb072ddf7b851df5e58f2b
Uula Ranta <uula@ranta.org>
2024-01-22 19:31:34 +0200

    Add compilemessages to build scripts

--- scripts/az_postbuild.sh
+++ scripts/az_postbuild.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+python manage.py compilemessages
 python manage.py compilescss --use-storage
 python manage.py collectstatic --noinput --ignore=*.scss
 python manage.py migrate --noinput
