6f1670bc5d2378b626ab19e78cc20edb09805540
Uula Ranta <uula@ranta.org>
2024-01-22 20:25:41 +0200

    Install gettext in runner, remove compile from postbuild

--- scripts/az_postbuild.sh
+++ scripts/az_postbuild.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-python manage.py compilemessages
 python manage.py compilescss --use-storage
 python manage.py collectstatic --noinput --ignore=*.scss
 python manage.py migrate --noinput
