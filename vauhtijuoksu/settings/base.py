74386ac8c3b091b23a47ffd628cf846e8ed754a6
Uula Ranta <uula@ranta.org>
2024-01-28 21:39:21 +0200

    Account & locale tweaks

--- vauhtijuoksu/settings/base.py
+++ vauhtijuoksu/settings/base.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    #'django.middleware.locale.LocaleMiddleware',
     'cms.middleware.user.CurrentUserMiddleware',
     'cms.middleware.page.CurrentPageMiddleware',
     'cms.middleware.toolbar.ToolbarMiddleware',
