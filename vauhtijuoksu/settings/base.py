aa0ffff25f7df7650e626444a5812031c9b65052
jiisloth <joonas.val@gmail.com>
2025-03-30 21:12:22 +0300

    Finish theme edits for releasing timetable for vj25

--- vauhtijuoksu/settings/base.py
+++ vauhtijuoksu/settings/base.py
@@ -250,6 +250,6 @@ BOOTSTRAP5 = {
 
 
 ## Vauhtijuoksu API
-VJ_API_URL = config('VJ_API_URL', 'http://127.0.0.1:5000')
+VJ_API_URL = config('VJ_API_URL', 'https://vjapi.jsloth.fi')
 #VJ_API_URL = config('VJ_API_URL', 'https://api.dev.vauhtijuoksu.fi')
 VJ_LEGACY_API_URL = config('VJ_LEGACY_API_URL', 'https://legacy.vauhtijuoksu.fi/api')
