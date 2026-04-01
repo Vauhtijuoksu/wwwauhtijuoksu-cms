b444f5782ce2103d984989e037d8abdd7468e891
jiisloth <joonas.val@gmail.com>
2026-04-01 12:51:28 +0300

    cherrypicked

--- vauhtijuoksu/settings/templates.py
+++ vauhtijuoksu/settings/templates.py
@@ -1,9 +1,5 @@
 # autogen default and fullscreen for year...
 yearly_templates = ["26", "25+", "25", "24", "23", "22", "21+"]
-
-# List of templates that can be used for CMS pages
-vauhtijuoksu_templates = []
-
 generic = [
     ('vauhtijuoksu/main/generic/default.html', 'VJ SideViuhti (Generic)'),
     ('vauhtijuoksu/main/generic/fullscreen.html', 'VJ SideViuhti Fullscreen (Generic)'),
@@ -14,13 +10,17 @@ additional = [
     ('vauhtijuoksu/main/deprecated/genericViolet.html', 'VJ OLD Generic violet theme'),
 ]
 
+# List of templates that can be used for CMS pages
+def vauhtijuoksu_templates():
+    templates = []
+    for t in generic:
+        templates.append(t)
 
-for t in generic:
-    vauhtijuoksu_templates.append(t)
+    for y in yearly_templates:
+        templates.append((f'vauhtijuoksu/main/{"vj" + y.replace("+", "plus")}/default.html', "VJ" + y + " theme default"))
+        templates.append((f'vauhtijuoksu/main/{"vj" + y.replace("+", "plus")}/fullscreen.html', "VJ" + y + " theme fullscreen"))
 
-for y in yearly_templates:
-    vauhtijuoksu_templates.append((f'vauhtijuoksu/main/{"vj" + y.replace("+", "plus")}/default.html', "VJ" + y + " theme default"))
-    vauhtijuoksu_templates.append((f'vauhtijuoksu/main/{"vj" + y.replace("+", "plus")}/fullscreen.html', "VJ" + y + " theme fullscreen"))
+    for t in additional:
+        templates.append(t)
+    return templates
 
-for t in additional:
-    vauhtijuoksu_templates.append(t)
